2468206dd81ab9719efeeba0921c0d80daffa1ac
fatur04 <72678244+fatur04@users.noreply.github.com>
2024-08-06 22:05:56 +0700

    Add files via upload

---
+++ app/Models/ActivityModels.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ActivityModels extends Model
+{
+    use HasFactory;
+    protected $table = "logactivity";
+    protected $primaryKey = "id_log";
+    protected $fillable = ['nama', 'inisial', 'activity', 'tanggal'];
+}
